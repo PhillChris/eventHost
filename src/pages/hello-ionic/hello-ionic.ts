14cf64106cb63f38db06937b1d1856a36b508b90
PhillChris <chrph@utschools.ca>
2018-02-21 14:39:16 -0500

    updated items class

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -10,23 +10,26 @@ import { ItemDetailsPage } from '../item-details/item-details';
 })
 export class HelloIonicPage {
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-  types: string[];
+  items: Array<{title: string, category: string, icon: string}>;
+  categories: string[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.icons = ['ios-school-outline', 'ios-briefcase-outline',
     'ios-star-outline', 'ios-american-football-outline'];
-    this.types = ['Games', 'Study Group', 'Types'];
 
     this.items = [];
-    /*this.items.push({title: 'Study Session', , icon: })*/
-    for(let i = 1; i < 11; i++) {
+    this.items.push({title: 'CLUBHOUSE SOCIAL', category: 'Promoted', icon: this.icons[2]})
+    this.items.push({title: 'Calculus Study Session', category: 'Education', icon: this.icons[0]})
+    this.items.push({title: 'Networking Session', category: 'Professional', icon: this.icons[1]})
+    this.items.push({title: 'LIT420 Office Hours', category: 'Education', icon: this.icons[0]})
+    this.items.push({title: 'Winter Touch Football', category: 'Leisure', icon: this.icons[3]})
+    /*for(let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Event ' + i,
         note: this.types[Math.floor(Math.random() * this.types.length)],
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
-    }
+    }*/
   }
 
   itemTapped(event, item) {
