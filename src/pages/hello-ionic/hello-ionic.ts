5b1fee68ae3b0aa3938df241faac54379c4cd3a3
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2018-02-22 13:17:49 -0500

    add searchbar

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -12,19 +12,43 @@ export class HelloIonicPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
   types: string[];
+  selectedItems: Array<{title: string, note: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
     this.types = ['Games', 'Study Group', 'Types'];
+    this.initializeItems();
+  }
 
+  initializeItems() {
     this.items = [];
+    this.selectedItems = [];
     for(let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Event ' + i,
         note: this.types[Math.floor(Math.random() * this.types.length)],
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
+    }
+    for(let i = 0; i < 10; i++) {
+      this.selectedItems.push(this.items[i])
+    }
+  }
+
+  getItems(searchbar: any) {
+    var query = searchbar.target.value.toLowerCase();
+
+    if (!query) {
+      this.selectedItems = this.items
+      return;
+    }
+
+    this.selectedItems = []
+    for(let i = 0; i < this.items.length; i++) {
+      if (this.items[i].title.toLowerCase().includes(query)) {
+        this.selectedItems.push(this.items[i]);
+      }
     }
   }
 
