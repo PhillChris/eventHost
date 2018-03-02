c59ecd1a6477515a59ea66044977aeebf642f056
PhillChris <chrph@utschools.ca>
2018-03-02 17:22:20 -0500

    added icon functionality for sumbit

--- src/pages/event-menu/event-menu.ts
+++ src/pages/event-menu/event-menu.ts
@@ -13,14 +13,28 @@ import { ItemDetailsPage } from '../item-details/item-details';
 export class EventMenu {
   constructor(public navCtrl: NavController, public events: Events) {
   }
+
   newEvent = {
     name: undefined,
-    description: undefined,
-    category: undefined
+    category: undefined,
+    description: undefined
   }
 
   makeEvent(event, name, category, description) {
-    this.events.publish('newEvent', {title: name, category: category, icon: 'ios-star-outline', categoryColor: '#78AB46', description: description})
+    console.log(category);
+    var icons = ['ios-school-outline', 'ios-american-football-outline', 'ios-briefcase-outline', 'ios-star-outline'];
+    var icon = undefined;
+    if (category === 'Education') {
+      icon = icons[0];
+    } else if (category === 'Leisure') {
+      icon = icons[1];
+    } else if (category === 'Professional') {
+      icon = icons[2];
+    } else {
+      icon = icons[3];
+    }
+
+    this.events.publish('newEvent', {title: name, category: category, icon: icon, categoryColor: '#78AB46', description: description})
     this.navCtrl.pop()
   }
 }
