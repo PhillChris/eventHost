c5ea3ebadb67d38660eaec22077901bd578609f4
PhillChris <chrph@utschools.ca>
2018-03-03 00:02:06 -0500

    almost final draft

--- src/pages/event-menu/event-menu.ts
+++ src/pages/event-menu/event-menu.ts
@@ -1,3 +1,5 @@
+declare var google
+
 import { Component } from '@angular/core';
 
 import { NavController, NavParams, Events } from 'ionic-angular';
@@ -11,17 +13,21 @@ import { ItemDetailsPage } from '../item-details/item-details';
   templateUrl: 'event-menu.html'
 })
 export class EventMenu {
+  latLng: any;
   constructor(public navCtrl: NavController, public events: Events) {
+    this.latLng = new google.maps.LatLng(50, 0);
   }
 
   newEvent = {
     name: undefined,
     category: undefined,
-    description: undefined
+    date: undefined,
+    time: undefined,
+    description: undefined,
+    address: undefined
   }
 
-  makeEvent(event, name, category, description) {
-    console.log(category);
+  makeEvent(event, name, date, time, category, description, address) {
     var icons = ['ios-school-outline', 'ios-american-football-outline', 'ios-briefcase-outline', 'ios-star-outline'];
     var icon = undefined;
     if (category === 'Education') {
@@ -34,7 +40,16 @@ export class EventMenu {
       icon = icons[3];
     }
 
-    this.events.publish('newEvent', {title: name, category: category, icon: icon, categoryColor: '#78AB46', description: description})
+    let geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status == 'OK') {
+      } else {
+        console.log('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+
+    var latLng = new google.maps.LatLng(lat_lng[0], lat_lng[1])
+    this.events.publish('newEvent', {title: name, category: category, icon: icon, startdate: date, latLng: latLng, starttime: time, categoryColor: '#78AB46', description: description})
     this.navCtrl.pop()
   }
 }
